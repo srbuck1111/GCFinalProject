f91362c2dd0f87a5434cd6f443f6a1e077f905b9
srbuck1111 <srbuck1111@gmail.com>
2019-12-17 13:55:55 -0500

    test

--- src/main/java/co/grandcircus/GCFinalProject/controllers/CharacterController.java
+++ src/main/java/co/grandcircus/GCFinalProject/controllers/CharacterController.java
@@ -3,15 +3,27 @@ package co.grandcircus.GCFinalProject.controllers;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.ModelAndView;
 
 import co.grandcircus.GCFinalProject.dndpojos.Classes;
+import co.grandcircus.GCFinalProject.dndpojos.PlayerCharacter;
+import co.grandcircus.GCFinalProject.model.User;
+import co.grandcircus.GCFinalProject.repo.CharacterRepo;
 
 @Controller
 public class CharacterController {
+	
+	@Autowired
+	HttpSession session;
+	
+	@Autowired
+	CharacterRepo cr;
 	
 	RestTemplate rt = new RestTemplate();
 
@@ -29,5 +41,13 @@ public class CharacterController {
 	}
 	
 	//character creation method that passes to character-select
+	@RequestMapping("add-character")
+	public ModelAndView addCharacter(PlayerCharacter pc) {
+		ModelAndView mv = new ModelAndView("characterSelect");
+		User loggedUser = (User) session.getAttribute("loggedUser");
+		
+		cr.save(pc);
+		return mv;
+	}
 	
 }
